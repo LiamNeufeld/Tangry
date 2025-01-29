e023fec33f621b5d61f095976cee7efca7113db8
Liam <l.e.neufeld2014@gmail.com>
2025-01-28 17:28:38 -0700

    Initial Code

---
+++ app/src/main/java/com/example/lab4_tangry/Shape.java
@@ -0,0 +1,15 @@
+package com.example.lab4_tangry;
+
+public abstract class Shape {
+    protected int x;
+    protected int y;
+
+    public Shape(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    // You can add abstract or concrete methods here, for example:
+    // public abstract void draw();
+}
+
