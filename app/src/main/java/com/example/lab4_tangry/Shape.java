b336abbdadd63e44fbd08ead27bd8395736b4cdb
Mabdalla-1608 <mabdalla@ualberta.ca>
2025-01-31 11:13:57 -0700

    Trying small change

--- app/src/main/java/com/example/lab4_tangry/Shape.java
+++ app/src/main/java/com/example/lab4_tangry/Shape.java
@@ -3,7 +3,7 @@ package com.example.lab4_tangry;
 public abstract class Shape {
     protected int x;
     protected int y;
-    protected String color = "blue";
+    protected String color = "turquoise_m";
 
 
     public Shape(int x, int y) {
