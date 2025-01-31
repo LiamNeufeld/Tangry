f268e49261f0459c4f6e2baaae7f023e11f0361a
Mazen Khafagy <mazen.khafagi@gmail.com>
2025-01-30 20:36:27 -0700

    teal

--- app/src/main/java/com/example/lab4_tangry/Shape.java
+++ app/src/main/java/com/example/lab4_tangry/Shape.java
@@ -4,6 +4,8 @@ public abstract class Shape {
     protected int x;
     protected int y;
 
+    protected String color = "teal";
+
     public Shape(int x, int y) {
         this.x = x;
         this.y = y;
